6165a9d106d668eefd7bc0a67eb999040554e881
John Nguyen <sineriner@gmail.com>
2020-01-24 01:02:00 -0500

    Added navigation to floating action button in Home screen

--- app/src/main/java/com/example/happinessperspective/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/happinessperspective/fragments/HomeFragment.kt
@@ -6,22 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 
 import com.example.happinessperspective.R
 import com.example.happinessperspective.databinding.HomeFragmentBinding
 import com.example.happinessperspective.viewModels.HomeViewModel
-import com.example.happinessperspective.viewModels.NewEntryViewModel
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: HomeFragmentBinding
     private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = HomeFragmentBinding.inflate(inflater)
+        binding = HomeFragmentBinding.inflate(inflater)
 
         return binding.root
     }
@@ -30,6 +32,16 @@ class HomeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
 
+        binding.viewModel = viewModel
+
+        viewModel.navigateToNewEntry.observe( viewLifecycleOwner,
+            Observer<Boolean> { shouldNavigate ->
+                if (shouldNavigate == true) {
+                    val navController = binding.root.findNavController()
+                    navController.navigate(R.id.action_homeFragment_to_newEntry)
+                    viewModel.onNavigatedCompleted()
+                }
+            })
     }
 
 }
--- app/src/main/java/com/example/happinessperspective/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/happinessperspective/viewModels/HomeViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.happinessperspective.viewModels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+    private val _navigateToNewEntry = MutableLiveData<Boolean>()
+    val navigateToNewEntry: LiveData<Boolean>
+        get() = _navigateToNewEntry
 
+    fun onFabClicked() {
+        _navigateToNewEntry.value = true
+    }
+
+    fun onNavigatedCompleted() {
+        _navigateToNewEntry.value = false
+    }
 }
