9efb0cc29c0d53a7fbcd0ed17e72d12bd4f4fbbd
John Nguyen <sineriner@gmail.com>
2020-01-25 11:42:55 -0500

    Added BindingAdapter file for RecyclerView (added "listData" binding)

---
+++ app/src/main/java/com/example/BindingAdapters.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.happinessperspective.database.Entry
+import com.example.happinessperspective.details.RecyclerViewAdapter
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<Entry>?) {
+    val adapter = recyclerView.adapter as RecyclerViewAdapter
+    adapter.submitList(data)
+}
