019ce9ecc46b9d18aba93222560f28f78a54ae04
John Nguyen <sineriner@gmail.com>
2020-01-25 13:23:28 -0500

    Added "getEntriesForSelectedMonth" query to Dao

--- app/src/main/java/com/example/happinessperspective/details/CurrentMonthDetailsViewModel.kt
+++ app/src/main/java/com/example/happinessperspective/details/CurrentMonthDetailsViewModel.kt
@@ -1,13 +1,18 @@
 package com.example.happinessperspective.details
 
 import android.app.Application
+import android.icu.util.Calendar
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.example.happinessperspective.database.Entry
 import com.example.happinessperspective.database.EntryDao
 
 class CurrentMonthDetailsViewModel(val dao: EntryDao, application: Application) : AndroidViewModel(application) {
-    private val _entries = dao.getAllEntries()
+    val c = Calendar.getInstance()
+    val currentYear = c.get(Calendar.YEAR)
+    val currentMonth = String.format("%02d", c.get(Calendar.MONTH) + 1)
+
+    private val _entries = dao.getEntriesForSelectedMonth(currentMonth)
     val entries : LiveData<List<Entry>>
         get() = _entries
 
