5afeb79b669c6a7c8978b9cacf1ab2fe13ee7efc
John Nguyen <sineriner@gmail.com>
2020-01-25 00:30:00 -0500

    Added entity data class and data access object (Dao) to database package

---
+++ app/src/main/java/com/example/happinessperspective/database/Entry.kt
@@ -0,0 +1,23 @@
+package com.example.happinessperspective.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "entry_table")
+data class Entry (
+    @PrimaryKey(autoGenerate = true)
+    var entryId: Int = 0,
+
+    var subject: String = "",
+
+    var note: String?,
+
+    var year: Int = -1,
+
+    var month: Int = -1,
+
+    var day: Int = -1,
+
+    var favorite: Boolean = false
+)
---
+++ app/src/main/java/com/example/happinessperspective/database/EntryDao.kt
@@ -0,0 +1,28 @@
+package com.example.happinessperspective.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface EntryDao {
+    @Insert
+    fun insert(input: Entry)
+
+    @Update
+    fun update(input: Entry)
+
+    @Query("SELECT * FROM entry_table WHERE entryId = :key")
+    fun get(key: Long): Entry?
+
+    @Query("DELETE FROM entry_table")
+    fun clear()
+
+    @Query("SELECT * FROM entry_table ORDER BY entryId DESC")
+    fun getAllEntries(): LiveData<List<Entry>>
+
+    @Query("SELECT * FROM entry_table ORDER BY entryId DESC LIMIT 1")
+    fun getMostRecentEntry(): Entry?
+}
