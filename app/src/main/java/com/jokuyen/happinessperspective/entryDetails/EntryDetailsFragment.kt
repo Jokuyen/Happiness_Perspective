4e8f5e7281604bf21f84256b871204d7c44abd37
John Nguyen <sineriner@gmail.com>
2020-02-05 19:01:37 -0500

    Small fixes: Removed popUpTo tags in navigation.xml

--- app/src/main/java/com/jokuyen/happinessperspective/entryDetails/EntryDetailsFragment.kt
+++ app/src/main/java/com/jokuyen/happinessperspective/entryDetails/EntryDetailsFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import com.jokuyen.happinessperspective.R
 import com.jokuyen.happinessperspective.database.EntryDatabase
 
